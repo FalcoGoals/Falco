17d304302d0aafd5029c1c0ffe635c88e254c146
Gerald <ngjingsiang@gmail.com>
2016-03-22 21:48:33 +0800

    added view controller for goal details

---
+++ Falco/GoalDetailViewController.swift
@@ -0,0 +1,19 @@
+//
+//  GoalDetailViewController.swift
+//  Falco
+//
+//  Created by Gerald on 22/3/16.
+//  Copyright © 2016 nus.cs3217.group04. All rights reserved.
+//
+
+import UIKit
+
+class GoalDetailViewController: UIViewController {
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+  }
+}
