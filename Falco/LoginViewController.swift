db08db94b73705274fe0d5693b8d44e6cfc3bf6b
John Yong <whipermr5@gmail.com>
2016-03-24 12:42:10 +0800

    Implemented subsequent auto login

--- Falco/LoginViewController.swift
+++ Falco/LoginViewController.swift
@@ -13,36 +13,59 @@ import FBSDKLoginKit
 class LoginViewController: UIViewController {
     @IBOutlet var loginButton: UIButton!
 
+    let ref = Firebase(url: "https://amber-torch-6648.firebaseio.com")
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        attemptLogin()
+    }
+
+    override func viewDidAppear(animated: Bool) {
+        if currentAccessToken() != nil {
+            self.loginButton.titleLabel?.text = "Logging in..."
+        }
     }
 
     @IBAction func loginTapped(sender: AnyObject) {
-        let ref = Firebase(url: "https://amber-torch-6648.firebaseio.com")
-        let facebookLogin = FBSDKLoginManager()
-        facebookLogin.logInWithReadPermissions(["email"], fromViewController: self, handler: {
+        FBSDKLoginManager().logInWithReadPermissions(["email"], fromViewController: self) {
             (facebookResult, facebookError) -> Void in
             if facebookError != nil {
+                self.loginButton.titleLabel?.text = "Login with Facebook"
                 print("Facebook login failed. Error \(facebookError)")
             } else if facebookResult.isCancelled {
                 print("Facebook login was cancelled.")
+                self.loginButton.titleLabel?.text = "Login with Facebook"
             } else {
                 self.loginButton.titleLabel?.text = "Login success!"
-                let accessToken = FBSDKAccessToken.currentAccessToken().tokenString
-                ref.authWithOAuthProvider("facebook", token: accessToken,
-                    withCompletionBlock: { error, authData in
-                        if error != nil {
-                            print("Login failed. \(error)")
-                        } else {
-                            print("Logged in! \(authData.providerData)")
-                        }
-                })
+                self.attemptLogin()
             }
-        })
+        }
     }
-    
+
+    private func attemptLogin() {
+        if let accessToken = currentAccessToken()?.tokenString {
+            ref.authWithOAuthProvider("facebook", token: accessToken) {
+                (error, authData) in
+                if error != nil {
+                    print("Login failed. \(error)")
+                } else {
+                    self.loginSuccess(authData)
+                }
+            }
+        }
+    }
+
+    private func loginSuccess(authData: FAuthData) {
+        print("Logged in as \(authData.providerData["displayName"]!)!")
+        self.loginButton.titleLabel?.text = "Login success!"
+        self.performSegueWithIdentifier("showMain", sender: nil)
+    }
+
+    private func currentAccessToken() -> FBSDKAccessToken? {
+        return FBSDKAccessToken.currentAccessToken()
+    }
 
     /*
     // MARK: - Navigation
