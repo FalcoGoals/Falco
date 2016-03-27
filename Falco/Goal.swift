7ae64e54e92e69fc9177add5444cde0c7160c351
Gerald <ngjingsiang@gmail.com>
2016-03-27 16:15:18 +0800

    made weight a computed property and remove observer for endTime and priority

--- Falco/Goal.swift
+++ Falco/Goal.swift
@@ -12,15 +12,10 @@ import UIKit
 class Goal: Hashable {
     var name: String
     var details: String
-    var endTime: NSDate {
-        didSet {
-            updateWeight()
-        }
-    }
-    var priority: PRIORITY_TYPE {
-        didSet {
-            updateWeight()
-        }
+    var endTime: NSDate
+    var priority: PRIORITY_TYPE
+    var weight: Int {
+        return priority.rawValue * 5 + 10
     }
 
     private var _uid: String
@@ -30,7 +25,6 @@ class Goal: Hashable {
 
     var identifier: String { return _uid }
     var goalType: GOAL_TYPE { return _goalType }
-    var weight: Int { return _weight }
 
     // consider adding time of creation?
     init(uid: String, name: String, details: String, endTime: NSDate, priority: PRIORITY_TYPE, goalType: GOAL_TYPE) {
@@ -40,14 +34,8 @@ class Goal: Hashable {
         self.priority = priority
         _uid = uid
         _goalType = goalType
-        updateWeight()
     }
 
-    /// Determines the weight of the goal based on priority and closeness of deadline
-    private func updateWeight() {
-        _weight = 50    //put this here first
-    }
-    
     var hashValue: Int {
         return _uid.hashValue
     }
