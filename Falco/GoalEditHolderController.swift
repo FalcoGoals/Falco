c8d096a11462df5d423563f7f0325f938fc1b015
Gerald <ngjingsiang@gmail.com>
2016-04-15 13:35:34 +0800

    added custom class for view controller holding the goal edit container view

---
+++ Falco/GoalEditHolderController.swift
@@ -0,0 +1,13 @@
+//
+//  GoalEditHolderController.swift
+//  Falco
+//
+//  Created by Gerald on 15/4/16.
+//  Copyright © 2016 nus.cs3217.group04. All rights reserved.
+//
+
+import UIKit
+
+class GoalEditHolderController: UIViewController {
+
+}
