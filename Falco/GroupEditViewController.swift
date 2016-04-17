f7c6a2727e6e54d4a7eacbac879364f9351a653e
Gerald <ngjingsiang@gmail.com>
2016-04-17 18:07:21 +0800

    added variable and fill in table view protocols

--- Falco/GroupEditViewController.swift
+++ Falco/GroupEditViewController.swift
@@ -9,7 +9,21 @@
 import UIKit
 
 class GroupEditViewController: UIViewController {
-    
+    var group: Group {
+        return delegate.getGroups().first!
+    }
+    var groupName: String {
+        return group.name
+    }
+    var members: [User] {
+        return group.members
+    }
+
+    var delegate: GroupModelDelegate!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
 }
 
 extension GroupEditViewController: UITableViewDelegate {
@@ -21,7 +35,19 @@ extension GroupEditViewController: UITableViewDataSource {
         return 1
     }
 
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return members.count
+    }
+
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        <#code#>
+        let cell = tableView.dequeueReusableCellWithIdentifier(Constants.groupMemberCellID, forIndexPath: indexPath) as! FriendTableViewCell
+        guard members.count > 0 else {
+            return cell
+        }
+
+        //set image and name
+        cell.setUser(members[indexPath.row])
+
+        return cell
     }
 }
