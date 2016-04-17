c859123926f44d09237a8aeafb31ab7f7bc0a5e4
Gerald <ngjingsiang@gmail.com>
2016-04-17 16:46:34 +0800

    added template code for table view controller

---
+++ Falco/GroupEditViewController.swift
@@ -0,0 +1,27 @@
+//
+//  GroupEditViewController.swift
+//  Falco
+//
+//  Created by Gerald on 17/4/16.
+//  Copyright © 2016 nus.cs3217.group04. All rights reserved.
+//
+
+import UIKit
+
+class GroupEditViewController: UIViewController {
+    
+}
+
+extension GroupEditViewController: UITableViewDelegate {
+
+}
+
+extension GroupEditViewController: UITableViewDataSource {
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        <#code#>
+    }
+}
